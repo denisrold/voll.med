e01c5e9c72ccdaef9f1dc5db4176fb3059fdd49e
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-06-28 11:39:38 -0300

    Security User Configuration

---
+++ voll-med/api/src/main/java/voll/med/api/domain/usuarios/DatosAutenticacionUsuario.java
@@ -0,0 +1,4 @@
+package voll.med.api.domain.usuarios;
+
+public record DatosAutenticacionUsuario(String login, String clave) {
+}
--- voll-med/api/src/main/java/voll/med/api/infra/security/SecurityConfigurations.java
+++ voll-med/api/src/main/java/voll/med/api/infra/security/SecurityConfigurations.java
@@ -2,6 +2,8 @@ package voll.med.api.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -16,5 +18,11 @@ public class SecurityConfigurations {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.csrf(csrf -> csrf.disable())
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).build();
- }}
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+}
 
