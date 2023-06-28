1a9cfc73250263c54e1ac7f13da613c35021fb8f
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-06-28 20:44:59 -0300

    Configuracion Enviroment variable

--- voll-med/api/src/main/java/voll/med/api/infra/security/TokenService.java
+++ voll-med/api/src/main/java/voll/med/api/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package voll.med.api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import voll.med.api.domain.usuarios.Usuario;
 
@@ -13,9 +14,11 @@ import java.time.ZoneOffset;
 @Service
 
 public class TokenService {
+    @Value("${api.security.secret}")
+    private String apiSecret;
     public String generarToken(Usuario usuario){
         try {
-            Algorithm algorithm = Algorithm.HMAC256("123456");
+            Algorithm algorithm = Algorithm.HMAC256(apiSecret);
            return JWT.create()
                     .withIssuer("voll.med")
                    .withSubject(usuario.getLogin())
