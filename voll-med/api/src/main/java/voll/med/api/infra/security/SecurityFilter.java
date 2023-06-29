f252c9dfd6a1275b3759c7408b4bcb7ef0a3a66d
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-06-29 07:18:11 -0300

    Configurando un filtro de authorizacion

--- voll-med/api/src/main/java/voll/med/api/infra/security/SecurityFilter.java
+++ voll-med/api/src/main/java/voll/med/api/infra/security/SecurityFilter.java
@@ -13,6 +13,6 @@ import java.io.IOException;
 public class SecurityFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-    System.out.println("El filtro deberia llamarse");
+   filterChain.doFilter(request,response);
     }
 }
